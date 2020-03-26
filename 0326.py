9dd4ca08d932bd54264002ee5b801b01ab414e0e
kuanchen0107 <a0939781983@gmail.com>
2020-03-26 19:33:38 +0800

    0326 pytohn練習

---
+++ 0326.py
@@ -0,0 +1,9 @@
+mathScorces = [60, 70, 10, 20, 81, 63, 4]
+mathScorces[2]
+mathScorces[0]
+mathScorces[-1]
+mathScorces[-2]
+print(mathScorces[2])
+print(mathScorces[0:6])
+print(mathScorces[-1])
+print(mathScorces[-2])
